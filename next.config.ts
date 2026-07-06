87cb97314b5cc6ea6d44c0f134ac07e8bd505ceb
Karthikeya Thallapally <208484803+KarthikeyaGSI@users.noreply.github.com>
2026-07-06 15:38:03 +0530

    Fix CSP: allow Google Maps iframe to load

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.clarity.ms https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://cdn.sanity.io https://www.facebook.com https://c.bing.com; media-src 'self' https://cdn.sanity.io; connect-src 'self' https://*.sanity.io https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://api.web3forms.com https://*.clarity.ms; frame-src 'self' https://www.youtube.com https://www.google.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.clarity.ms https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://cdn.sanity.io https://www.facebook.com https://c.bing.com https://maps.gstatic.com https://maps.googleapis.com; media-src 'self' https://cdn.sanity.io; connect-src 'self' https://*.sanity.io https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://api.web3forms.com https://*.clarity.ms; frame-src 'self' https://www.youtube.com https://www.google.com https://maps.google.com https://www.google.com/maps;",
           }
         ],
       },
